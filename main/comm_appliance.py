14ff575b5e8a84b741a9a2daa36f5c9d4157dda4
DebugConnoisseur <67976720+DebugConnoisseur@users.noreply.github.com>
2022-02-16 19:17:41 +0530

    Create comm_appliance.py

---
+++ main/comm_appliance.py
@@ -0,0 +1,16 @@
+def comm_appliance():
+     if menyoo==3:
+        print("----------------------------Wattage of Common Appliances---------------------------")
+        print ("""No. Appliance                      Min    Max    Standby
+        print 1.  32 InchLEDTV                    20W    60W    1W
+        print 2.  Refrigerator                    100W   200W   N/A
+        print 3.  Washing Machine                 500W   500W   1W
+        print 4.  100W light bulb (Incandescent)  100W   100W   0W
+        print 5.  Electric Pressure Cooker        1000W  1000W  N/A
+        print 6.  Inverter Air conditioner        1300W  1800W  N/A
+        print 7.  Iron                            1000W  1000W  N/A
+        print 8.  Laptop Computer                 50W    100W   N/A
+        print 9.  LED Light Bulb                  7W     10W    0W
+        print 10.  Pedestal Fan                   50W    60W    N/A""")
+    else:
+        pass 
