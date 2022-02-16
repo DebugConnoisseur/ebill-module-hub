82e79e1aaf77026a91a50647fad76add9c132e96
DebugConnoisseur <67976720+DebugConnoisseur@users.noreply.github.com>
2022-02-16 19:15:06 +0530

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,19 @@
+def menu(a):
+   if menyoo==1:
+      print(3)
+   elif menyoo==2:
+      print(2)
+   elif menyoo==3:
+      print(4)
+   elif menyoo==4:
+      print(66)
+   elif menyoo==5:
+      print(48)
+print("------------------------------Main Menu-----------------------------")
+print("1.Calculate a electricity bill")
+print("2.View previous bills")
+print("3.View wattage of common appliances")
+print("4.Calculator")
+print("5.Exit the application")
+menyoo=(int(input("Enter your choice(1-5):  ")))
+menu(menyoo)
