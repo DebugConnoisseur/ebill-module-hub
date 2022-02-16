dcad5949bf80f45245a155ec3ed08a9f6e88383c
DebugConnoisseur <67976720+DebugConnoisseur@users.noreply.github.com>
2022-02-16 19:16:57 +0530

    Create softinfo.py

---
+++ main/softinfo.py
@@ -0,0 +1,8 @@
+def softinfo():
+    if menyoo==6:
+        print("-----------------------------Software Information----------------------")
+        print("Version: 1.1")
+        print("Last Patch: 15/2/2022")
+        print("Python version: 3.9")
+    else:
+         pass
